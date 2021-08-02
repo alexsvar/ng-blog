7cc94ac225c69b7bdb40f7c54428e7ae28fb7120
Alexander Morozov <alexsvar@icloud.com>
2021-08-02 15:58:01 +0300

    Added list of posts and it's filtering

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core'
+import {Subscription} from 'rxjs'
+
+import {PostsService} from '../../shared/posts.service'
+import {Post} from '../../shared/interfaces'
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = []
+  pSub: Subscription
+  searchStr = ''
+
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
   }
 
+  remove(id: string) {
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
+  }
 
 }
