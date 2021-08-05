5bd826a302ca4ae8749cc0aa0c342a0f71470e48
Alexander Morozov <alexsvar@icloud.com>
2021-08-05 13:23:15 +0300

    Added pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule, Provider} from '@angular/core'
 import {HTTP_INTERCEPTORS} from '@angular/common/http'
 import {registerLocaleData} from '@angular/common'
 import ruLocale from '@angular/common/locales/ru'
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
@@ -11,7 +12,8 @@ import {HomePageComponent} from './home-page/home-page.component'
 import {PostPageComponent} from './post-page/post-page.component'
 import {PostComponent} from './shared/components/post/post.component'
 import {SharedModule} from './shared/shared.module'
-import {AuthInterceptor} from './shared/auth.interceptor'
+import {AuthInterceptor} from './shared/auth.interceptor';
+import { environment } from '../environments/environment'
 
 
 registerLocaleData(ruLocale, 'ru')
@@ -33,7 +35,13 @@ const INTERCEPTOR_PROVIDER: Provider = {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent]
